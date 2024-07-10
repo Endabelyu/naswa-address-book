22bb7d575d869311a1b0ce025ff01a18b7e9652f
naswa <endabelyuproject@gmail.com>
2024-07-10 17:46:33 +0700

    feat(tailwindConfig): add new boxshadow theme with key full

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       boxShadow: {
         sideright: ' 2px 0px 2px 0px rgba(0,0,0,0.75)',
         sidebot: ' box-shadow: 0px 6px 7px 0px rgba(0,0,0,0.75)',
+        full: '0px 0px 7px 0px rgba(0,0,0,0.75)',
       },
     },
   },
